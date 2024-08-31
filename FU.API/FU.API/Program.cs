a3324208962166ce300d0d6018b7db4c55e0c60b
James Pretorius <jamesmpretorius@gmail.com>
2024-08-31 09:13:25 -0400

    Remove HSTS suggestion comment

--- FU.API/FU.API/Program.cs
+++ FU.API/FU.API/Program.cs
@@ -30,7 +30,6 @@ internal class Program
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
         else
